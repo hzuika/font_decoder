8c3b38b2988c13827e8721940f13b11f969ad40c
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-08-30 00:00:33 +0900

    add fvar tag

--- src/data_types.rs
+++ src/data_types.rs
@@ -31,7 +31,7 @@ impl fmt::Display for Tag {
 }
 
 pub const NAME: Tag = Tag::from_be_bytes(*b"name");
-
+pub const FVAR: Tag = Tag::from_be_bytes(*b"fvar");
 // 32-bit signed fixed-point number (16.16)
 pub struct Fixed(pub i32);
 
