f02619a5d8f36ede8e58540eba65aee7d3f8bd65
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-08-29 15:03:36 +0900

    name table を列挙するスクリプトをTTCに対応させる

--- src/bin/enum_names.rs
+++ src/bin/enum_names.rs
@@ -1,8 +1,22 @@
 use std::{fs::File, io::Read};
 
-use font_decoder::{data_types::NAME, name::NameTable, table::Table};
+use font_decoder::{
+    data_types::NAME,
+    name::NameTable,
+    table::{is_ttc, Collection, Table},
+};
 
-// cargo run --bin all_name_table
+fn callback(table: &Table) {
+    let name = table.get_table_data(&NAME).unwrap();
+    let name = NameTable::parse(name).unwrap();
+    for record in &name.name_records {
+        dbg!(&record);
+        let string = name.get_string(&record);
+        dbg!(&string);
+    }
+}
+
+// cargo run --bin enum_names
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if let Some(filepath) = args.get(1) {
@@ -10,13 +24,13 @@ fn main() {
         let mut buffer = Vec::new();
         file.read_to_end(&mut buffer).unwrap();
 
-        let table = Table::new(&buffer).unwrap();
-        let name = table.get_table_data(&NAME).unwrap();
-        let name = NameTable::parse(name).unwrap();
-        for record in &name.name_records {
-            dbg!(&record);
-            let string = name.get_string(&record).unwrap();
-            dbg!(&string);
+        if is_ttc(&buffer).unwrap() {
+            let collection = Collection::new(&buffer).unwrap();
+            for i in 0..collection.header.num_fonts as usize {
+                callback(&collection.get(i).unwrap())
+            }
+        } else {
+            callback(&Table::new(&buffer).unwrap())
         }
     } else {
         println!("filepath is necessary")
