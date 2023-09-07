74d2154e1d693c0a266b10019e29bcd9e4e7aca9
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-09-07 22:48:05 +0900

    name table の family name, subfamily name を列挙するスクリプト

---
+++ src/bin/enum_subfamily_name.rs
@@ -0,0 +1,42 @@
+use std::{fs::File, io::Read};
+
+use font_decoder::{
+    data_types::NAME,
+    name::NameTable,
+    table::{is_ttc, Collection, Table},
+};
+
+fn callback(table: &Table) {
+    let name = table.get_table_data(&NAME).unwrap();
+    let name = NameTable::parse(name).unwrap();
+    for record in name
+        .nameRecords
+        .into_iter()
+        .filter(|x| x.nameId.0 == 2 || x.nameId.0 == 17 || x.nameId.0 == 22)
+    {
+        dbg!(&record.nameId);
+        let string = name.get_string(&record).unwrap();
+        dbg!(&string);
+    }
+}
+
+// cargo run --bin enum_names
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if let Some(filepath) = args.get(1) {
+        let mut file = File::open(filepath).unwrap();
+        let mut buffer = Vec::new();
+        file.read_to_end(&mut buffer).unwrap();
+
+        if is_ttc(&buffer).unwrap() {
+            let collection = Collection::new(&buffer).unwrap();
+            for i in 0..collection.header.numFonts as usize {
+                callback(&collection.get(i).unwrap())
+            }
+        } else {
+            callback(&Table::new(&buffer).unwrap())
+        }
+    } else {
+        println!("filepath is necessary")
+    }
+}
