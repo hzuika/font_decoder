2c4f7072a7b3237297487ee4ca77d8984ed1b173
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-09-09 02:47:40 +0900

    cmap の code_point が char になったことによる変更

--- src/bin/enum_cmap.rs
+++ src/bin/enum_cmap.rs
@@ -11,24 +11,20 @@ fn main() {
     file.read_to_end(&mut buf).unwrap();
     let table = Table::new(&buf).unwrap();
     let cmap = table.get_cmap_table().unwrap();
+    dbg!(&cmap.header);
     for item in &cmap.header.encodingRecords {
         dbg!(&item);
         match cmap.get_subtable(&item) {
             Some(subtable) => {
                 let map = subtable.get_code_point_glyph_id_map();
-                let mut map: Vec<(u16, u16)> = map.into_iter().collect();
+                let mut map: Vec<(char, u16)> = map.into_iter().collect();
                 dbg!(map.len());
                 map.sort_by(|a, b| a.0.cmp(&b.0));
                 for (code_point, glyph_id) in map {
-                    let char = std::char::from_u32(code_point as u32);
-                    match char {
-                        Some(char) => {
-                            println!("U+{:06X} ('{:?}') => {}", code_point, char, glyph_id);
-                        }
-                        None => {
-                            panic!("invalid unicode character or surrogate {:x}", code_point);
-                        }
-                    }
+                    println!(
+                        "U+{:06X} ('{:?}') => {}",
+                        code_point as u32, code_point, glyph_id
+                    );
                 }
             }
             None => {}
