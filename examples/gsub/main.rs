380877f642cb3db498d952bed07f7533f8449b87
Yuki Hashimoto <39191601+hzuika@users.noreply.github.com>
2023-12-31 13:03:51 +0900

    update example

--- examples/gsub/main.rs
+++ examples/gsub/main.rs
@@ -1,11 +1,26 @@
-use font_decoder::table::Table;
+use std::io::Read;
 
-fn main() {
-    let data = include_bytes!("../../NunitoSans_10pt-Black.ttf");
-    let table = Table::new(data).unwrap();
+use font_decoder::table::{is_ttc, Table};
+
+fn callback(table: &Table) {
     if let Some(gsub) = table.get_gsub_table() {
         for feature_record in gsub.feature_list.featureRecords {
             println!("{}", feature_record.featureTag);
         }
+    }
+}
+
+fn main() {
+    // let data = include_bytes!("../../NunitoSans_10pt-Black.ttf");
+    let args: Vec<String> = std::env::args().collect();
+    if let Some(filepath) = args.get(1) {
+        let mut file = std::fs::File::open(filepath).unwrap();
+        let mut buf = vec![];
+        file.read_to_end(&mut buf).unwrap();
+        if is_ttc(&buf).unwrap() {
+        } else {
+            callback(&Table::new(&buf).unwrap());
+        }
+    } else {
     }
 }
